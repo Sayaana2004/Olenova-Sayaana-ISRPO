da43fdaf894f63078b6a38aab4811901a8c131eb
Sayaana2004 <112675608+Sayaana2004@users.noreply.github.com>
2022-10-27 08:32:21 +0900

    Add files via upload

---
+++ Page1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = ToursEntities.GetContext().Hotels.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
